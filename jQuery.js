8c4bef9dfe5efb6bcadd5eb3bdaebd0642926a3f
Michell-117 <michell.ra47@gmail.com>
2021-11-09 14:20:56 -0600

    formulario

--- jQuery.js
+++ jQuery.js
@@ -1,3 +1,41 @@
 $(document).ready(function(){
     // alert("jQuery cargado correctamente");
 })
+/*
+$(document).ready(function(){
+    alert("jQuery cargado - seccion 28 02_Efectos en jQuery");
+
+    var caja1 = $("#caja1");
+    caja1.hide();
+
+    var caja2 = $("#caja2");
+    caja2.hide();
+
+    var caja3 = $("#caja3");
+
+    var mostrar = $("#mostrar");
+    var ocultar = $("#ocultar");
+    var palanca = $("#palanca");
+
+    ocultar.hide();
+    mostrar.click(function(){
+        $(this).hide();
+        ocultar.show("normal");
+        caja1.show("fast");
+        caja2.fadeIn("slow");
+    });
+
+    ocultar.click(function(){
+        $(this).hide();
+        mostrar.show("normal");
+        caja1.hide("fast");
+        caja2.fadeOut("slow");
+    });
+
+    palanca.click(function(){
+        caja3.fadeToggle();
+    })
+
+    // Todos estos efectos los podemos ver mejor en https://api.jquery.com/category/effects/
+
+});*/
